5e4dc73f76ce25609a6b9a7a97af3ff5b308c736
wowangki <wowangki@gmail.com>
2018-12-20 03:57:55 +0900

    18/12/20 UI 작업본

---
+++ Unity Team Portfolio/Assets/Resources/UI/02.Scripts/BtnCtrl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class BtnCtrl : MonoBehaviour, IPointerUpHandler, IPointerDownHandler {
+
+    private Text text;
+    private Image icon;
+    private readonly float textOffset = 5.0f;
+    private readonly float iconOffset = 3.0f;
+
+    // Use this for initialization
+    void Start () {
+        text = GetComponentInChildren<Text>();
+        icon = transform.GetChild(0).GetComponent<Image>();
+	}
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        if (text) text.transform.Translate(0, -textOffset, 0);
+        if (icon) icon.transform.Translate(0, -iconOffset, 0);
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (text) text.transform.Translate(0, textOffset, 0);
+        if (icon) icon.transform.Translate(0, iconOffset, 0);
+    }
+}
---
+++ Unity Team Portfolio/Assets/Resources/UI/02.Scripts/UIMgr.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class UIMgr : MonoBehaviour
+{
+    enum LUIMenu
+    {
+        MAIN,
+        ADDON,
+        STATUS,
+        INVENTORY,
+        QUEST
+    }
+
+    private List<GameObject> vLMenu;
+    private readonly Vector2Int setScreenRatio = new Vector2Int(16, 9);
+    private void Awake()
+    {
+        Screen.SetResolution(Screen.width, Screen.width * setScreenRatio.x / setScreenRatio.y, true);
+    }
+
+    private void Start()
+    {
+        vLMenu = GameObject.FindGameObjectsWithTag("LUI_Panel").ToList();
+
+        for (int i=0; i<vLMenu.Count; i++)
+        {
+            vLMenu[i].SetActive(i == (int)LUIMenu.MAIN ? true : false);
+        }
+    }
+
+    public void OpenCloseAddOnMenu()
+    {
+        if (!((int)LUIMenu.MAIN < vLMenu.Count || (int)LUIMenu.ADDON < vLMenu.Count)) return;
+
+        vLMenu[(int)LUIMenu.MAIN].SetActive(!vLMenu[(int)LUIMenu.MAIN].activeSelf);
+        vLMenu[(int)LUIMenu.ADDON].SetActive(!vLMenu[(int)LUIMenu.ADDON].activeSelf);
+    }
+
+    public void OpenCloseStatus()
+    {
+        if (!((int)LUIMenu.ADDON < vLMenu.Count || (int)LUIMenu.STATUS < vLMenu.Count)) return;
+
+        vLMenu[(int)LUIMenu.ADDON].SetActive(!vLMenu[(int)LUIMenu.ADDON].activeSelf);
+        vLMenu[(int)LUIMenu.STATUS].SetActive(!vLMenu[(int)LUIMenu.STATUS].activeSelf);
+    }
+
+    public void OpenCloseInventory()
+    {
+        if (!((int)LUIMenu.ADDON < vLMenu.Count || (int)LUIMenu.INVENTORY < vLMenu.Count)) return;
+
+        vLMenu[(int)LUIMenu.ADDON].SetActive(!vLMenu[(int)LUIMenu.ADDON].activeSelf);
+        vLMenu[(int)LUIMenu.INVENTORY].SetActive(!vLMenu[(int)LUIMenu.INVENTORY].activeSelf);
+    }
+
+    public void OpenCloseQuest()
+    {
+        if (!((int)LUIMenu.ADDON < vLMenu.Count || (int)LUIMenu.QUEST < vLMenu.Count)) return;
+
+        vLMenu[(int)LUIMenu.ADDON].SetActive(!vLMenu[(int)LUIMenu.ADDON].activeSelf);
+        vLMenu[(int)LUIMenu.QUEST].SetActive(!vLMenu[(int)LUIMenu.QUEST].activeSelf);
+    }
+}
